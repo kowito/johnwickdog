0efcaca3eb03ec845785f9152865996587a17328
Kowit Charoenratchatabhan <hi@kowito.com>
2021-10-28 04:41:39 +0700

    fix path

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Hero from '../component/hero.js'
+import Hero from '../components/Hero/hero.jsx'
 
 export default function Home() {
   return (
