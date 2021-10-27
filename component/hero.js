1172d7970c3d3f43ff80a8b14474da36f4b6f5e1
Kowit Charoenratchatabhan <hi@kowito.com>
2021-10-28 03:33:06 +0700

    init commit

---
+++ component/hero.js
@@ -0,0 +1,131 @@
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+import { ChevronRightIcon, StarIcon } from '@heroicons/react/solid'
+import Image from 'next/image'
+
+
+export default function Hero() {
+  return (
+    <div className="bg-white pb-8 sm:pb-12 lg:pb-12">
+      <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-48">
+        <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
+          <div>
+            <div>
+              <Image
+                src="/icon.png"
+                alt="Picture of the author"
+                width={120}
+                height={120}
+              />
+
+
+            </div>
+            <div className="mt-20">
+              <div>
+                <a href="#" className="inline-flex space-x-4">
+                  <span className="rounded bg-indigo-50 px-2.5 py-1 text-xs font-semibold text-indigo-600 tracking-wide uppercase">
+                    What's new
+                  </span>
+                  <span className="inline-flex items-center text-sm font-medium text-indigo-600 space-x-1">
+                    <span>Just shipped version 0.1.0</span>
+                    <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                  </span>
+                </a>
+              </div>
+              <div className="mt-6 sm:max-w-xl">
+                <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                  John Wick Dog Token
+                </h1>
+                <p className="mt-6 text-xl text-gray-500">
+                  A Decentralized Meme Token that Evolved into a Serial Killer<br />
+                  Contract address :
+                    <a href="https://bscscan.com/token/0xf70ed3f37953bddac3d21cb3ec3731af14532211" target="_blank">
+                      0xf70ed3f37953bddac3d21cb3ec3731af14532211
+                    </a>
+
+                </p>
+              </div>
+              <form action="#" className="mt-12 sm:max-w-lg sm:w-full sm:flex">
+                
+                <div className="mt-4 sm:mt-0 sm:ml-3">
+                  <a
+                    href="https://pancakeswap.finance/swap?inputCurrency=BNB&outputCurrency=0xf70ed3f37953bddac3d21cb3ec3731af14532211"
+                    className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-600 text-base font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10"
+                    target="_blank"
+                  >
+                    Buy now
+                  </a>
+                </div>
+              </form>
+              <div className="mt-6">
+                <div className="inline-flex items-center divide-x divide-gray-300">
+                  <div className="flex-shrink-0 flex pr-5">
+                    <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+                    <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+                    <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+                    <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+                    <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+                  </div>
+                  <div className="min-w-0 flex-1 pl-5 py-1 text-sm text-gray-500 sm:py-3">
+                    <span className="font-medium text-gray-900">Rated 5 stars</span> by over{' '}
+                    <span className="font-medium text-indigo-600">500 beta users</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="sm:mx-auto sm:max-w-3xl sm:px-6">
+          <div className="py-12 sm:relative sm:mt-12 sm:py-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+            <div className="hidden sm:block">
+              <div className="absolute inset-y-0 left-1/2 w-screen bg-gray-50 rounded-l-3xl lg:left-80 lg:right-0 lg:w-full" />
+              <svg
+                className="absolute top-8 right-1/2 -mr-3 lg:m-0 lg:left-0"
+                width={404}
+                height={392}
+                fill="none"
+                viewBox="0 0 404 392"
+              >
+                <defs>
+                  <pattern
+                    id="837c3e70-6c3a-44e6-8854-cc48c737b659"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  </pattern>
+                </defs>
+                <rect width={404} height={392} fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)" />
+              </svg>
+            </div>
+            <div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12">
+              <img
+                className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
+                src="https://images.unsplash.com/photo-1608113240010-e76286118bf2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=637&q=80"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
